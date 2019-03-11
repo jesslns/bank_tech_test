c192d98b3e2189a9e1773d106c17e6110a8f72ed
jesslns <jesslns@yahoo.com.hk>
2019-03-11 12:27:20 +0000

    Initialize Account with zero balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,7 +5,11 @@ describe Account do
   subject(:account) { described_class.new }
 
   it "can take deposit" do
-    expect(account).to respond_to(:deposit).with(1).argument 
+    expect(account).to respond_to(:deposit).with(1).argument
+  end
+
+  it "starts with zero balance" do
+    expect(account.balance).to eq(0)
   end
 
 end
