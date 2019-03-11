354603c602a5883b45266cb8ebe840348021b401
jesslns <jesslns@yahoo.com.hk>
2019-03-11 16:31:46 +0000

    Add statement instance variables to Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,15 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :statement
 
   def initialize
     @balance = 0
+    @statement = []
   end
 
   def deposit(amount)
     @balance += amount
+    @statement << {:time => Time.now.strftime("%Y/%m/%d"), :debit => amount, :balance => @balance}
   end
 
   def withdraw(amount)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,9 @@ require "account"
 describe Account do
 
   subject(:account) { described_class.new }
+  before do
+    @statement = double(:statement)
+  end
 
   it "starts with zero balance" do
     expect(account.balance).to eq(0)
@@ -15,6 +18,12 @@ describe Account do
   it "can withdaw money" do
     account.deposit(1)
     expect{account.withdraw(1)}.to change{account.balance}.by(-1)
+  end
+
+  it "records transaction in the statement" do
+    account.deposit(1)
+    allow(account).to receive(:statement).and_return([@statement])
+    expect(account.statement[-1]).to eq(@statement)
   end
 
 end
