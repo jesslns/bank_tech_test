6288d43d4d4a71bee7a1a1490b4dd2e4f532b3ed
jesslns <jesslns@yahoo.com.hk>
2019-03-11 14:42:44 +0000

    Add date method to Statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,2 +1,9 @@
 class Statement
+
+  attr_reader :date
+
+  def initialize (time = Time.now)
+    @date = time.strftime("%Y/%m/%d")
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,5 +1,12 @@
 require "statement"
 
 describe Statement do
-  
+  subject(:statement) { described_class.new }
+  before do
+    @time = "2019-03-11 14:25:58 +0000"
+  end
+  it "can store date" do
+    allow(statement).to receive(:initialize).and_return(@time)
+    expect(statement.date).to eq("2019/03/11")
+  end
 end
