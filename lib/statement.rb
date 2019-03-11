6528b622b65c69d903afc1e42e34f3229a8b9c44
jesslns <jesslns@yahoo.com.hk>
2019-03-11 14:53:52 +0000

    Add balance method to Statement, and removed credit and debit parameters in the initialize method

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,11 +1,12 @@
 class Statement
 
-  attr_reader :date, :credit, :debit
+  attr_reader :date, :credit, :debit, :balance
 
-  def initialize (time = Time.now, credit = credit, debit = debit)
+  def initialize (time = Time.now)
     @date = time.strftime("%Y/%m/%d")
     @credit = credit
     @debit = debit
+    @balance = balance
   end
 
 end
