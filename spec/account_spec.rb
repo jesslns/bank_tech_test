3e5371c342ab76a5b9ffc6a0b4936d02fce8f165
jesslns <jesslns@yahoo.com.hk>
2019-03-11 12:20:17 +0000

    Add deposit method that can take one argument

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,11 @@
 require "account"
 
 describe Account do
-  
+
+  subject(:account) { described_class.new }
+
+  it "can take deposit" do
+    expect(account).to respond_to(:deposit).with(1).argument 
+  end
+
 end
